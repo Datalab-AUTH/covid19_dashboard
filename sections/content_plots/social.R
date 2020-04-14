5871b4deb8f4cd89335e18c5f706ea4cf44683b5
George Vlahavas <vlahavas@gmail.com>
2020-04-14 14:36:57 +0300

    Add correlation and per capita to Human Freedom
    Add correlation results and per capita checkbox (default=checked) to
    human freedom plot.

--- sections/content_plots/social.R
+++ sections/content_plots/social.R
@@ -14,8 +14,26 @@ output$freedom <- renderPlotly({
     ) %>%
     as.data.frame()
   data_conf$iso3c <- countrycode(data_conf$Country, origin = "country.name", destination = "iso3c")
-  data_conf$values <- 100000 * data_conf$values / data_conf$population
 
+  if (input$case_freedom_var == "confirmed") {
+    y_label = "# Confirmed Cases"
+    dot_color = "#FC8D62"
+  } else if (input$case_freedom_var == "recovered") {
+    y_label = "# Recovered"
+    dot_color = "E78AC3"
+  } else if (input$case_freedom_var == "deceased") {
+    y_label = "# Deceased"
+    dot_color = "#8DA0CB"
+  } else { # active cases
+    y_label = "# Active Cases"
+    dot_color = "#66C2A5"
+  }
+  
+  if (input$checkbox_cases_freedom_per_capita) {
+    y_label = paste(y_label, "/ 100,000 people")
+    data_conf$values <- 100000 * data_conf$values / data_conf$population
+  }
+  
   data_hf <- data_human_freedom %>%
     select("ISO_code", "hf_score") %>%
     rename("iso3c" = "ISO_code") %>%
@@ -24,19 +42,14 @@ output$freedom <- renderPlotly({
   data_merged <- merge(data_conf, data_hf)
   data_merged$Country <- countrycode(data_merged$iso3c, origin = "iso3c", destination = "country.name")
   
-  if (input$case_freedom_var == "confirmed") {
-    y_label = "# Confirmed Cases / 100,000 people"
-    dot_color = "#FC8D62"
-  } else if (input$case_freedom_var == "recovered") {
-    y_label = "# Recovered / 100,000 people"
-    dot_color = "E78AC3"
-  } else if (input$case_freedom_var == "deceased") {
-    y_label = "# Deceased / 100,000 people"
-    dot_color = "#8DA0CB"
-  } else { # active cases
-    y_label = "# Active Cases / 100,000 people"
-    dot_color = "#66C2A5"
-  }
+  correlation <- cor.test(data_merged$values, data_merged$hf_score, method = "spearman")
+  p_value <- correlation$p.value
+  rho <- round(correlation$estimate, 3)
+  if (p_value < 0.001) p_value_text = "P < 0.001"
+  else if (p_value < 0.01) p_value_text = "P < 0.01"
+  else p_value_text = paste("P = ", round(p_value, 3))
+  p_value_text <- paste("Correlation r =", rho, ",", p_value_text)
+  
   p <- plot_ly(
     data_merged,
     x     = ~hf_score,
@@ -50,7 +63,17 @@ output$freedom <- renderPlotly({
     layout(
       yaxis = list(title = y_label, type = "log"),
       xaxis = list(title = "Human Freedom Score"),
-      showlegend = FALSE
+      showlegend = FALSE,
+      annotations = list(
+        showarrow = FALSE,
+        xref = "paper",
+        x = 1,
+        yref = "paper",
+        y = 1,
+        text = paste(p_value_text),
+        bordercolor = "#FFFFFF",
+        bgcolor = "#444B55"
+      )
     )
   
   p <- layout(p,
@@ -239,6 +262,15 @@ output$box_case_social <- renderUI({
         column(
           uiOutput("select_cases_freedom_variable"),
           width = 3
+        ),
+        column(
+          checkboxInput(
+            "checkbox_cases_freedom_per_capita",
+            label = "Per Capita",
+            value = TRUE
+          ),
+          width = 3,
+          style = "float: right; padding: 10px; margin-right: 50px"
         ),
         width = 6
       )
