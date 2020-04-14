1633773b0305934341625010afe127bfc24c10f6
George Vlahavas <vlahavas@gmail.com>
2020-04-14 15:41:52 +0300

    Add OECD immunization to influenza plot

--- sections/content_plots/social.R
+++ sections/content_plots/social.R
@@ -218,6 +218,120 @@ output$select_cases_healthGDP_limit <- renderUI((
 ))
 
 #
+# Immunization to influenza
+#
+
+output$oecd_influenza <- renderPlotly({
+  req(input$case_oecd_influenza_var)
+  req(input$case_oecd_influenza_limit)
+  
+  data_conf <- data_latest %>%
+    select("Country/Region", "population", input$case_oecd_influenza_var) %>%
+    rename("Country" = "Country/Region") %>%
+    rename("values" = input$case_oecd_influenza_var) %>%
+    group_by(Country) %>%
+    summarise(
+      "population" = sum(population, na.rm = T),
+      "values" = sum(values, na.rm = T)
+    ) %>%
+    filter(values >= as.numeric(input$case_oecd_influenza_limit)) %>%
+    as.data.frame()
+  data_conf$iso3c <- countrycode(data_conf$Country, origin = "country.name", destination = "iso3c")
+  
+  if (input$case_oecd_influenza_var == "confirmed") {
+    y_label = "# Confirmed Cases"
+    dot_color = "#FC8D62"
+  } else if (input$case_oecd_influenza_var == "recovered") {
+    y_label = "# Recovered"
+    dot_color = "E78AC3"
+  } else if (input$case_oecd_influenza_var == "deceased") {
+    y_label = "# Deceased"
+    dot_color = "#8DA0CB"
+  } else { # active cases
+    y_label = "# Active Cases"
+    dot_color = "#66C2A5"
+  }
+  
+  if (input$checkbox_oecd_influenza_per_capita) {
+    y_label = paste(y_label, "/ 100,000 people")
+    data_conf$values <- 100000 * data_conf$values / data_conf$population
+  }
+  
+  data_merged <- merge(data_conf, data_oecd)
+  data_merged$Country <- countrycode(data_merged$iso3c, origin = "iso3c", destination = "country.name")
+  
+  correlation <- cor.test(data_merged$values, data_merged$influenzaImmunization, method = "spearman")
+  p_value <- correlation$p.value
+  rho <- round(correlation$estimate, 3)
+  if (p_value < 0.001) p_value_text = "P < 0.001"
+  else if (p_value < 0.01) p_value_text = "P < 0.01"
+  else p_value_text = paste("P = ", round(p_value, 3))
+  p_value_text <- paste0("Correlation r = ", rho, ", ", p_value_text)
+  
+  p <- plot_ly(
+    data_merged,
+    x     = ~influenzaImmunization,
+    y     = ~values,
+    name  = ~Country,
+    type  = 'scatter',
+    mode  = 'markers',
+    marker = list(
+      color = dot_color
+    )
+  ) %>%
+    layout(
+      yaxis = list(title = y_label, type = "log"),
+      xaxis = list(title = "Immunization to Influenza"),
+      showlegend = FALSE,
+      annotations = list(
+        showarrow = FALSE,
+        xref = "paper",
+        x = 1,
+        yref = "paper",
+        y = 1,
+        text = paste(p_value_text),
+        bordercolor = "#FFFFFF",
+        bgcolor = "#444B55"
+      )
+    )
+  
+  p <- layout(p,
+              font = list(color = "#FFFFFF"),
+              paper_bgcolor = "#444B55",
+              plot_bgcolor = "#444B55",
+              yaxis = list(
+                zerolinecolor = "#666666",
+                linecolor = "#999999",
+                gridcolor = "#666666"
+              ),
+              xaxis = list(
+                zerolinecolor = "#666666",
+                linecolor = "#999999",
+                gridcolor = "#666666"
+              )
+  )
+  return(p)
+})
+
+output$select_cases_oecd_influenza_variable <- renderUI({
+  selectizeInput(
+    "case_oecd_influenza_var",
+    label    = "Select Variable",
+    choices  = list("Confirmed" = "confirmed", "Recovered" = "recovered", "Deceased" = "deceased", "Active" = "active"),
+    multiple = FALSE
+  )
+})
+
+output$select_cases_oecd_influenza_limit <- renderUI((
+  selectizeInput(
+    "case_oecd_influenza_limit",
+    label = "Number of cases",
+    choices = list("≥1" = 1, "≥10" = 10, "≥100" = 100, "≥1,000" = 1000, "≥10,000" = 10000),
+    multiple = FALSE
+  )
+))
+
+#
 # Plot layout
 #
 
@@ -264,6 +378,34 @@ output$box_case_social <- renderUI({
         column(
           checkboxInput(
             "checkbox_cases_freedom_per_capita",
+            label = "Per Capita",
+            value = TRUE
+          ),
+          width = 3,
+          style = "float: right; padding: 10px; margin-right: 50px"
+        ),
+        width = 6
+      )
+    ),
+    fluidRow(
+      box(
+        title = "Cases vs Immunization to Influenza",
+        plotlyOutput("oecd_influenza"),
+        column(
+          width = 3,
+          style = "float: right; padding: 10px; margin-right: 50px"
+        ),
+        column(
+          uiOutput("select_cases_oecd_influenza_variable"),
+          width = 3
+        ),
+        column(
+          uiOutput("select_cases_oecd_influenza_limit"),
+          width = 3
+        ),
+        column(
+          checkboxInput(
+            "checkbox_oecd_influenza_per_capita",
             label = "Per Capita",
             value = TRUE
           ),
