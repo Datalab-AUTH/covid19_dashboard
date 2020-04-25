962f203277c0635f0594200ab734307c5e9b86ac
George Vlahavas <vlahavas@gmail.com>
2020-04-25 21:18:42 +0300

    Add a tile for active cases

--- sections/content_overview/keyFigures.R
+++ sections/content_overview/keyFigures.R
@@ -2,6 +2,7 @@ sumData <- function(date) {
   if (date >= min(data_evolution$date)) {
     data <- data_atDate(date) %>% summarise(
       confirmed = sum(confirmed, na.rm = T),
+      active    = sum(active, na.rm = T),
       recovered = sum(recovered, na.rm = T),
       deceased  = sum(deceased, na.rm = T),
       countries = n_distinct(`Country/Region`)
@@ -17,6 +18,7 @@ key_figures <- reactive({
 
   data_new <- list(
     new_confirmed = (data$confirmed - data_yesterday$confirmed) / data_yesterday$confirmed * 100,
+    new_active    = (data$active - data_yesterday$active) / data_yesterday$active * 100,
     new_recovered = (data$recovered - data_yesterday$recovered) / data_yesterday$recovered * 100,
     new_deceased  = (data$deceased - data_yesterday$deceased) / data_yesterday$deceased * 100,
     new_countries = data$countries - data_yesterday$countries
@@ -24,6 +26,7 @@ key_figures <- reactive({
 
   keyFigures <- list(
     "confirmed" = HTML(paste(format(data$confirmed, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_confirmed))),
+    "active"    = HTML(paste(format(data$active, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_active))),
     "recovered" = HTML(paste(format(data$recovered, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_recovered))),
     "deceased"  = HTML(paste(format(data$deceased, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_deceased))),
     "countries" = HTML(paste(format(data$countries, big.mark = " "), "/ 195", sprintf("<h4>(%+d)</h4>", data_new$new_countries)))
@@ -41,6 +44,15 @@ output$valueBox_confirmed <- renderValueBox({
   )
 })
 
+output$valueBox_active <- renderValueBox({
+  valueBox(
+    key_figures()$active,
+    subtitle = "Active",
+    icon     = icon("stethoscope"),
+    color    = "light-blue",
+    width    = NULL
+  )
+})
 
 output$valueBox_recovered <- renderValueBox({
   valueBox(
@@ -73,10 +85,11 @@ output$box_keyFigures <- renderUI(box(
   title = paste0("Key Figures (", strftime(input$timeSlider, format = "%d.%m.%Y"), ")"),
   fluidRow(
     column(
-      valueBoxOutput("valueBox_confirmed", width = 3),
-      valueBoxOutput("valueBox_recovered", width = 3),
-      valueBoxOutput("valueBox_deceased", width = 3),
-      valueBoxOutput("valueBox_countries", width = 3),
+      valueBoxOutput("valueBox_confirmed", width = 2),
+      valueBoxOutput("valueBox_active", width = 2),
+      valueBoxOutput("valueBox_recovered", width = 2),
+      valueBoxOutput("valueBox_deceased", width = 2),
+      valueBoxOutput("valueBox_countries", width = 2),
       width = 12,
       style = "margin-left: -20px"
     )
