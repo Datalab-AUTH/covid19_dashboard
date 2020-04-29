5c8bf52a267872c1794d763ca847aa974fc2f750
George Vlahavas <vlahavas@gmail.com>
2020-04-30 01:51:09 +0300

    Convert data_greece df to tibble

--- process_data.R
+++ process_data.R
@@ -407,7 +407,9 @@ if (data_greece_all["status_code"] == 200 &&
   data_greece <- data_greece_all_parsed %>%
     merge(data_greece_ICU_parsed) %>%
     merge(data_greece_total_tests_parsed) %>%
-    arrange(date)
+    arrange(date) %>%
+    as_tibble() %>%
+    mutate(date = as.Date(date, format="%Y-%m-%d"))
   saveRDS(data_greece, "data/data_greece_all.RDS")
 }
 data_greece_cumulative <- GET("https://covid-19-greece.herokuapp.com/total")
