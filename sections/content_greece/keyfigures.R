b750fce8fbea85394284cddfbb4c9878292cd346
George Vlahavas <vlahavas@gmail.com>
2020-04-30 01:51:42 +0300

    Show more info in key figure tiles

--- sections/content_greece/keyfigures.R
+++ sections/content_greece/keyfigures.R
@@ -5,6 +5,8 @@ key_figures_greece <- reactive({
   data_yesterday <- data_greece_all %>%
     slice(n() - 1)
 
+  
+  
   data_new <- list(
     new_confirmed = (data$confirmed - data_yesterday$confirmed) / data_yesterday$confirmed * 100,
     new_active    = (data$active - data_yesterday$active) / data_yesterday$active * 100,
@@ -21,6 +23,8 @@ key_figures_greece <- reactive({
     "deceased"  = HTML(paste(format(data$deaths, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_deaths))),
     "icu"       = HTML(paste(format(data$icu, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_icu))),
     "tests"     = HTML(paste(format(data$tests, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_tests))),
+    "case_age"  = HTML(paste(format(data_greece_age_averages[["case"]], big.mark = " "), sprintf("<h4>years</h4>"))),
+    "death_age" = HTML(paste(format(data_greece_age_averages[["death"]], big.mark = " "), sprintf("<h4>years</h4>"))),
     "date"      = data$date
   )
   return(keyFigures)
@@ -82,20 +86,52 @@ output$valueBox_greece_tests <- renderValueBox({
   )
 })
 
+output$valueBox_greece_age_case <- renderValueBox({
+  valueBox(
+    key_figures_greece()$case_age,
+    subtitle = "Average age for confirmed cases",
+    icon     = icon("notes-medical"),
+    color    = "light-blue"
+  )
+})
+
+output$valueBox_greece_age_death <- renderValueBox({
+  valueBox(
+    key_figures_greece()$death_age,
+    subtitle = "Average age for fatalities",
+    icon     = icon("hospital"),
+    color    = "light-blue"
+  )
+})
+
 output$box_keyFigures_greece <- renderUI(box(
   title = paste0("Key Figures (", strftime(key_figures_greece()$date, format = "%d.%m.%Y"), ")"),
   fluidRow(
     column(
-      valueBoxOutput("valueBox_greece_confirmed", width = 2),
-      valueBoxOutput("valueBox_greece_active", width = 2),
-      valueBoxOutput("valueBox_greece_recovered", width = 2),
-      valueBoxOutput("valueBox_greece_deceased", width = 2),
-      valueBoxOutput("valueBox_greece_icu", width = 2),
-      valueBoxOutput("valueBox_greece_tests", width = 2),
+      valueBoxOutput("valueBox_greece_confirmed", width = 3),
+      valueBoxOutput("valueBox_greece_active", width = 3),
+      valueBoxOutput("valueBox_greece_recovered", width = 3),
+      valueBoxOutput("valueBox_greece_deceased", width = 3),
       width = 12,
       style = "margin-left: -20px"
     )
   ),
-  div("Last updated: ", strftime(changed_date, format = "%d.%m.%Y - %R %Z")),
+  fluidRow(
+    column(
+      valueBoxOutput("valueBox_greece_icu", width = 3),
+      valueBoxOutput("valueBox_greece_tests", width = 3),
+      valueBoxOutput("valueBox_greece_age_case", width = 3),
+      valueBoxOutput("valueBox_greece_age_death", width = 3),
+      width = 12,
+      style = "margin-left: -20px"
+    )
+  ),
+  div(
+    "Last updated: ", strftime(changed_date, format = "%d/%m/%Y - %R %Z. "),
+    "Data provided by the",
+    tags$a(href = "https://www.covid19response.gr/",
+           "COVID-19 Response Greece"),
+    "project."
+      ),
   width = 12
 ))
