48ec782eedfd4fe791d2d5729e8035a6dda96c02
George Vlahavas <vlahavas@gmail.com>
2021-12-30 00:52:31 +0200

    Use new script to pull data

--- run.sh
+++ run.sh
@@ -6,25 +6,17 @@
 # latest data every hour or so. It is supposed to be used from inside
 # the Docker container as the default command.
 
-pull_latest_data() {
-	(
-	 echo "Updating JHU data"
-	 cd data/JHU_data
-	 git pull origin master
-	 echo "Updating Oxford data"
-	 cd ../../data/oxford_data
-	 git pull origin master
-	 cd ../..
-	 echo "Updating RDS files"
-	 Rscript ./process_data.R
-	)
-}
 
-pull_latest_data
+pull_data() {
+	./pull_latest_data.sh
+	Rscript ./process_data.R
+)
+
+pull_data
 (
  while true; do
 	sleep 3600 # 1 hour
-	pull_latest_data
+	pull_data
  done
 ) &
 
